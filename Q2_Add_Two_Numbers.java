3b9d22d78ddb66accc99799e5dc60683afca359f
masterujjval <guptarickey3@gmail.com>
2025-01-08 14:08:20 +0530

    Q2 Done and Executed Successfully

---
+++ Q2_Add_Two_Numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Add{
+	public static void main(String []args){
+int x,y;
+
+Scanner input=new Scanner(System.in);
+System.out.println("Enter 1st integer");
+x=input.nextInt();
+
+System.out.println("Enter 2nd integer");
+y=input.nextInt();
+System.out.println("The sum of two numbers are:");
+System.out.println(x+y);
+	}
+}
+
